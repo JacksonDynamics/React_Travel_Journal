3f7aadb04a50903275e4f52621507ea6d837582c
studmanj <bjackso3@hotmail.com>
2022-09-16 19:10:20 -0600

    Header Icon

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,12 @@
 function Header() {
     return(
-        <header className="header">my travel journal</header>
+        <>
+
+        <header className="header">
+            <i className="fa-solid fa-earth-americas header--icon"></i>
+            <h3 className="header--title">my travel journal</h3>
+        </header>
+        </>
     )
 }
 
