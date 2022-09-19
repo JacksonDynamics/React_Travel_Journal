8be1ae17a0f6d8ddd68b5e1d3e45d80fbd2437b6
studmanj <bjackso3@hotmail.com>
2022-09-19 15:42:56 -0600

    Props

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import Header from "./components/Header"
 import Card from "./components/Card"
 
+let germany = {
+  location: germany,
+  title: Germany,
+  description: description
+}
+
 function App() {
   return (
     <div className="App">
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,11 @@
 
-function Card() {
+function Card(props) {
     return(
         <div>
-            <img src="./images/hero-bg.jpg" width="400px" alt=""/>
-            <h1>Card</h1>
+            <img src="./images/hero-bg.jpg" width="200px" alt=""/>
+            <h1>{props.location}</h1></h1>
+            <h2 className="title">{props.title}</h2>
+            <h4 className="description">{props.description}</h4>
         </div>
     )
 }
