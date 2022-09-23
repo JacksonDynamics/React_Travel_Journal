41c323d5596e27ee36da8471c38e72bb5341cdcf
studmanj <bjackso3@hotmail.com>
2022-09-22 18:47:27 -0600

    Card formatting

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,20 +1,44 @@
 
 function Card(props) {
     return(
-        <div className="card">
-            <div className="card-content">
-                <img src={`./images/${props.coverImg}`} 
-                    className="card--image" 
-                    alt={props.alt}/>
-                <div className="card-intro">
-                    <h1 className="card--location">{props.location}</h1>
-                    <a href={props.map}    
-                    className="card--maps">View on Google Maps</a>
-                </div>
-                <h2 className="title">{props.title}</h2>
-                <h4 className="description">{props.description}</h4>     
-            </div>
-        </div>
+        // <div className="card">
+        //     <div className="card-content">
+        //         <img src={`./images/${props.coverImg}`} 
+        //             className="card--image" 
+        //             alt={props.alt}/>
+        //         <div className="card-intro">
+        //             <h1 className="card--location">{props.location}</h1>
+        //             <a href={props.map}    
+        //             className="card--maps">View on Google Maps</a>
+        //         </div>
+        //         <h2 className="title">{props.title}</h2>
+        //         <h4 className="description">{props.description}</h4>     
+        //     </div>
+        // </div>
+
+
+<div className="card">
+    <img className="card--img" 
+         src={`./images/${props.coverImg}`} 
+         alt={props.alt} 
+        />
+
+    <div className="card--intro">
+        <h2 className="card--location">{props.location}</h2>
+        <a href={props.map}>
+            View on Google Maps
+        </a>
+
+        <h3 className="card--title">{props.title}</h3>
+        <p className="card--date">
+            {props.dates}
+        </p>
+        <p className="description">{props.description}</p>
+    </div>
+</div>
+
+
+
     )
 }
 
