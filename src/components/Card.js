80f4aaed33b98703fc6889fdc266031bd6c309d9
studmanj <bjackso3@hotmail.com>
2022-09-23 19:30:50 -0600

    Dynamic Border

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,49 +1,30 @@
 
 function Card(props) {
     return(
-        // <div className="card">
-        //     <div className="card-content">
-        //         <img src={`./images/${props.coverImg}`} 
-        //             className="card--image" 
-        //             alt={props.alt}/>
-        //         <div className="card-intro">
-        //             <h1 className="card--location">{props.location}</h1>
-        //             <a href={props.map}    
-        //             className="card--maps">View on Google Maps</a>
-        //         </div>
-        //         <h2 className="title">{props.title}</h2>
-        //         <h4 className="description">{props.description}</h4>     
-        //     </div>
-        // </div>
+        <div className="card">
+            <img className="card--img" 
+                src={`./images/${props.coverImg}`} 
+                alt={props.alt} 
+                />
 
+            <div className="card--intro">
+                <div className="card--heading">
+                    <h2 className="card--location">
+                        <i className="fa-sharp fa-solid fa-location-dot"></i>{props.location}
+                    </h2>
+                    <a className="card--location" href={props.map}>
+                        View on Google Maps
+                    </a>
+                </div>
+                <h3 className="card--title">{props.title}</h3>
+                <p className="card--date">
+                    {props.startDate} - {props.endDate}
+                </p>
+                <p className="card--description">{props.description}</p>
 
-<div className="card">
-    <img className="card--img" 
-         src={`./images/${props.coverImg}`} 
-         alt={props.alt} 
-        />
-
-    <div className="card--intro">
-        <div className="card--heading">
-            <h2 className="card--location">
-                <i className="fa-sharp fa-solid fa-location-dot"></i>{props.location}
-            </h2>
-            <a className="card--location" href={props.map}>
-                View on Google Maps
-            </a>
+            </div>
+                
         </div>
-        <h3 className="card--title">{props.title}</h3>
-        <p className="card--date">
-            {props.startDate} - {props.endDate}
-        </p>
-        <p className="card--description">{props.description}</p>
-
-    </div>
-        
-</div>
-
-
-
     )
 }
 
